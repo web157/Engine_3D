b532cbdc6a220af9778a73daeedb0fb5c94de5e6
Rid <Rid@PC>
2016-03-29 00:28:56 +0300

    begin

---
+++ System/System_.js
@@ -0,0 +1,25 @@
+
+function System_(gl)
+{
+      
+}
+
+System_.prototype.Initalize = function()
+{
+    
+};
+
+System_.prototype.Run = function()
+{
+     
+};
+
+System_.prototype.ArrTexture = function()
+{        
+   
+};
+
+System_.prototype.ArrObj = function()
+{
+    
+};
