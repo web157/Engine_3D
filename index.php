f3a35dd26d90a2c52b03ba288b6f2ddee9ac5f0e
Rid <Rid@PC>
2016-03-29 02:10:17 +0300

    begin

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </head>
     <body>
         
-        <canvas id="canvas3D" width="1280" height="909">Ваш браузер не поддерживает элемент canvas</canvas>
+        <canvas id="canvas3D">Ваш браузер не поддерживает элемент canvas</canvas>
   
         <script id="shader-fs" type="x-shader/x-fragment">
 
@@ -40,9 +40,113 @@
             }
         </script>
     
-    <script type="text/javascript">
+    <script type="text/javascript"> 
         
+ var Time_1 = 0;
+ var Time_2 = 0;
+                  
+var PosX = Array();
+var PosZ = Array();
+var PosT = Array();
+
+var PosMouse = 0;
+
+PosX[0] = 0.0;
+PosZ[0] = 0.0;
+PosT[0] = 0.0;
+
+var date = new Date();
+
+       gl = new Object();
+    
+    var canvas = document.getElementById("canvas3D");
+    
+    canvas.width = $(document).width();
+    canvas.height = $(document).height();
+    
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");       
+    }
+    catch(e) {}
+   
+      if (!gl) {
+        alert("Ваш браузер не поддерживает WebGL");
+      }
+    if(gl){
+          document.addEventListener('keydown', handleKeyDown, false);       
+          document.addEventListener('mousemove', handleMouseDown, false);
+          
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+                
+        var sys_ = new System_(gl);
+        
+        sys_.Initalize();
+        
+        window.onload=function(){
+
+            (function animloop(){
+                
+                //Time_1 = date.getMilliseconds();
+                
+                sys_.Run(PosX, PosZ, PosT, PosMouse);
+                
+                //Time_2 = Time_1 - Time_2;
             
+              requestAnimFrame(animloop, canvas);
+            })();
+
+        };
+    }  
+        function handleKeyDown(e){
+    switch(e.keyCode)
+    {
+        case 65:  // стрелка вправо
+            PosT[0]+=0.2;
+           
+            break;
+        case 68:  // стрелка влево
+            PosT[0]-=0.2;
+            
+         
+            break;
+        case 87:  // стрелка вниз
+           PosZ[0] += Math.cos(PosT[0]) * 0.5;
+           PosX[0] += Math.sin(PosT[0]) * 0.5;
+         
+            break;
+        case 83:  // стрелка вверх
+            PosZ[0] -= Math.cos(PosT[0]) * 0.5;
+            PosX[0] -= Math.sin(PosT[0]) * 0.5;
+       
+            break;
+    }   
+         
+}
+
+var temp = 0;
+var temp1 = 0;
+function handleMouseDown(e){
+  
+    temp = e.clientX;
+      
+      if(temp > temp1){ PosMouse+=0.05; }
+      
+      if(temp < temp1){ PosMouse-=0.05; }
+        
+    temp1 = temp;    
+}
+        
+        window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     ||
+         function(callback, element) {
+           return window.setTimeout(callback, 1000/60);
+         };
+})();
     </script>
         
     </body>
