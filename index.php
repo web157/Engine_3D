8de9026b0f27a9600caedd4f038442909b1bb8e8
Rid <Rid@PC>
2016-03-29 00:28:16 +0300

    begin

--- index.php
+++ index.php
@@ -1,17 +1,49 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8">       
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script type="text/javascript" src="System/System_.js"></script>
+        <script type="text/javascript" src="WebGL/WebGL.js"></script>
+        <script  type="text/javascript" src="gl-matrix-min.js"></script>
+       
+        <title>Engine_3D</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+        <canvas id="canvas3D" width="1280" height="909">Ваш браузер не поддерживает элемент canvas</canvas>
+  
+        <script id="shader-fs" type="x-shader/x-fragment">
+
+            precision highp float;
+            uniform sampler2D uSampler;
+            varying vec2 vTextureCoords;
+
+            void main(void) {
+              gl_FragColor = texture2D(uSampler, vTextureCoords);
+            }
+        </script>
+
+        <script id="shader-vs" type="x-shader/x-vertex">
+
+            attribute vec3 aVertexPosition;
+            attribute vec2 aVertexTextureCoords;
+            varying vec2 vTextureCoords;
+            uniform mat4 uMVMatrix;
+            uniform mat4 uPMatrix;
+
+            void main(void) {
+              gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+              vTextureCoords = aVertexTextureCoords;
+
+            }
+        </script>
+    
+    <script type="text/javascript">
+        
+            
+    </script>
+        
     </body>
 </html>
